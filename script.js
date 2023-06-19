22b912057ab70f4f22a2f0b78bace6ffc3b76ee5
Camila Fernandes Assunção <129509305+CamilaFAssuncao@users.noreply.github.com>
2023-06-19 12:10:18 +0200

    starters collection

---
+++ script.js
@@ -0,0 +1,33 @@
+const itemsObject = [
+    {
+        itemPic: "mushroom.png",
+        itemName: "Shimeji and Shitake",
+        itemTags: ["vegan", "vegetarian", "dairyfree", "gluten-free"],
+        itemDescription: ["Stir-fry mushrooms in olive oil with saké and soy sauce."],
+        itemPrice: "6e",  },
+
+    {
+            itemPic: "sunomono.png",
+            itemName: "Sunomono",
+            itemTags: ["vegan", "vegetarian", "dairyfree", "gluten-free"],
+            itemDescription: ["Cumcuber salad marinated in rice vinegar."],
+            itemPrice: "4e",  },
+
+    {
+                itemPic: "sweetpotato.png",
+                itemName: ["Sweet potato chips"],
+                itemTags: ["vegan", "vegetarian", "dairyfree", "gluten-free"],
+                itemDescription: ["Baked sweet potato chips."],
+                itemPrice: "4e",  },
+
+    {
+                    itemPic: "shrimp.png",
+                    itemName: ["Honey Garlic and Pineapple Shrimp Skewers"],
+                    itemTags: ["dairyfree", "gluten-free"],
+                    itemDescription: ["Succulent shrimp and juicy pineapple chunks grilled, coated in a sweet and savory honey garlic glaze"],
+                    itemPrice: "8e",  }
+            
+        
+    
+
+]
