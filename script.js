b0fa1b1547d6d06b9b6cd0b31fc23ce2306a1e76
iammmei <safae.khess@gmail.com>
2023-06-19 12:29:46 +0200

    dish object

---
+++ script.js
@@ -0,0 +1,29 @@
+let dishesObject = [
+  {
+    itemPic: "salmonpoke.png",
+    itemName: "Salmon Poke ",
+    itemTags: ["fish"],
+    itemDescription: "Choice of base, fresh salmon, mango or pineapple, radish, cucumber, red cabbage, avocado, edamame, sesame seeds.Served with the sauce of your choice.",
+    itemPrice: "12,95" },
+
+{
+itemPic: "tunapoke.png",
+    itemName: "Tuna Poke",
+    itemTags: ["fish"],
+    itemDescription: "Choice of marinated tuna, mango or pineapple, radish, cucumber, red cabbage, avocado, edamame, sesame seeds.Served with the sauce of your choice.",
+    itemPrice: "12,95" },
+
+{
+itemPic: "falafelpoke.png",
+    itemName: "Falafel Poke ",
+    itemTags: ["vegetarian","gluten-free"],
+    itemDescription: "Choice of base, falafels depending , grenade , radish, carrot, avocado, edamame, cucumber, red cabbage, sesame seeds and raspberry.Served with the sauce of your choice",
+    itemPrice: "12,95",  },
+
+{
+itemPic: "veggiechicken.png",
+    itemName: "Veggie Chicken Poke ",
+    itemTags: ["vegan","vegetarian","gluten-free"],
+    itemDescription: "Vinegared rice base, veggie chicken, avocado, mango, edamame, cucumber, cashew nuts, thai cebette and sesame.",
+    itemPrice: "12,95",  }
+]
