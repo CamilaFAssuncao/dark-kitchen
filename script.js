6772a81e4c8b632e74581c4bbfbab2e6b7e57caf
iammmei <safae.khess@gmail.com>
2023-06-19 12:33:13 +0200

    update tags

--- script.js
+++ script.js
@@ -2,28 +2,36 @@ let dishesObject = [
   {
     itemPic: "salmonpoke.png",
     itemName: "Salmon Poke ",
-    itemTags: ["fish"],
-    itemDescription: "Choice of base, fresh salmon, mango or pineapple, radish, cucumber, red cabbage, avocado, edamame, sesame seeds.Served with the sauce of your choice.",
-    itemPrice: "12,95" },
+    itemTags: ["main course", "fish"],
+    itemDescription:
+      "Choice of base, fresh salmon, mango or pineapple, radish, cucumber, red cabbage, avocado, edamame, sesame seeds.Served with the sauce of your choice.",
+    itemPrice: "12,95",
+  },
 
-{
-itemPic: "tunapoke.png",
+  {
+    itemPic: "tunapoke.png",
     itemName: "Tuna Poke",
-    itemTags: ["fish"],
-    itemDescription: "Choice of marinated tuna, mango or pineapple, radish, cucumber, red cabbage, avocado, edamame, sesame seeds.Served with the sauce of your choice.",
-    itemPrice: "12,95" },
+    itemTags: ["main course", "fish"],
+    itemDescription:
+      "Choice of marinated tuna, mango or pineapple, radish, cucumber, red cabbage, avocado, edamame, sesame seeds.Served with the sauce of your choice.",
+    itemPrice: "12,95",
+  },
 
-{
-itemPic: "falafelpoke.png",
+  {
+    itemPic: "falafelpoke.png",
     itemName: "Falafel Poke ",
-    itemTags: ["vegetarian","gluten-free"],
-    itemDescription: "Choice of base, falafels depending , grenade , radish, carrot, avocado, edamame, cucumber, red cabbage, sesame seeds and raspberry.Served with the sauce of your choice",
-    itemPrice: "10,95",  },
+    itemTags: ["main course", "vegetarian", "gluten-free"],
+    itemDescription:
+      "Choice of base, falafels depending , grenade , radish, carrot, avocado, edamame, cucumber, red cabbage, sesame seeds and raspberry.Served with the sauce of your choice",
+    itemPrice: "10,95",
+  },
 
-{
-itemPic: "veggiechicken.png",
+  {
+    itemPic: "veggiechicken.png",
     itemName: "Veggie Chicken Poke ",
-    itemTags: ["vegan","vegetarian","gluten-free"],
-    itemDescription: "Vinegared rice base, veggie chicken, avocado, mango, edamame, cucumber, cashew nuts, thai cebette and sesame.",
-    itemPrice: "11,95",  }
-]
+    itemTags: ["main course", "vegan", "vegetarian", "gluten-free"],
+    itemDescription:
+      "Vinegared rice base, veggie chicken, avocado, mango, edamame, cucumber, cashew nuts, thai cebette and sesame.",
+    itemPrice: "11,95",
+  },
+];
